04d924b94919c3c8bd345b5b3b2572fb759a227b
Youngseo Kang <youngseo.kangg@gmail.com>
2025-09-08 12:11:26 +0900

    creating routes and initial page.tsx for routes

---
+++ src/app/(afterLogin)/explore/page.tsx
@@ -0,0 +1,8 @@
+export default function ExplorePage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Explore Page</h1>
+      <p>여기는 탐색 페이지입니다.</p>
+    </div>
+  );
+}
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function AfterLoginLayout({ children }: Props) {
+  return <div>애프터 로그인 {children}</div>;
+}
