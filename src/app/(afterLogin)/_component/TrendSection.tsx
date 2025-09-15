08e716bb5d2fe4508519b48af3de4b0c06b4da37
Youngseo Kang <youngseo.kangg@gmail.com>
2025-09-15 10:08:29 +0900

    implement pathname-based conditional rendering for right section (search input, search filters, trend section)

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,17 @@
-import style from "./trendSection.module.css";
+"use client";
+
+// component
 import Trend from "@/app/(afterLogin)/_component/Trend";
 
+// style
+import style from "./trendSection.module.css";
+import { usePathname } from "next/navigation";
+
 export default function TrendSection() {
+  const pathname = usePathname();
+
+  if (pathname === "/explore") return null;
+
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
