b713b5363767bf8936a2f6fa06e27fd3e4353275
Youngseo Kang <youngseo.kangg@gmail.com>
2025-09-19 09:22:56 +0900

    creating common MSWComponent and changing before-login/SignupModal into a server component

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import { ChangeEventHandler, FormEventHandler, useState } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import z from "zod";
 
 // component
@@ -25,109 +22,84 @@ const signupSchema = z.object({
 });
 
 export default function SignupModal() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [image, setImage] = useState("");
-  const [imageFile, setImageFile] = useState<File>();
-  const [message, setMessage] = useState("");
-  const router = useRouter();
+  const onSubmit = async (formData: FormData) => {
+    "use server";
 
-  const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setId(e.target.value);
-  };
-
-  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setPassword(e.target.value);
-  };
-  const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setNickname(e.target.value);
-  };
-  const onChangeImageFile: ChangeEventHandler<HTMLInputElement> = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      const file = e.target.files[0];
-      setImageFile(file);
-      setImage(URL.createObjectURL(file)); // 미리보기 URL 생성
+    if (!formData.get("id")) {
+      return { message: "no_id" };
     }
-  };
-
-  const onSubmit: FormEventHandler = (e) => {
-    e.preventDefault();
-
-    // 입력값 검증
-    const result = signupSchema.safeParse({ id, nickname, password, image });
-
-    if (!result.success) {
-      const errorMsgs = result.error.issues.map((issue) => issue.message);
-      setMessage(errorMsgs.length > 0 ? errorMsgs[0] : "에러가 발생했습니다.");
-
-      return false;
+    if (!formData.get("name")) {
+      return { message: "no_name" };
+    }
+    if (!formData.get("password")) {
+      return { message: "no_password" };
+    }
+    if (!formData.get("image")) {
+      return { message: "no_image" };
     }
 
-    // fetch("http://localhost:9090/api/users", {
-    //   method: "post",
-    //   body: JSON.stringify({
-    //     id,
-    //     nickname,
-    //     image,
-    //     password,
-    //   }),
-    //   credentials: "include",
-    // })
-    //   .then((response: Response) => {
-    //     console.log(response.status);
-    //     if (response.status === 200) {
-    //       router.replace("/home");
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
+    let shouldRedirect = false;
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/users`,
+        {
+          method: "post",
+          body: formData,
+          credentials: "include",
+        }
+      );
+      if (response.status === 403) {
+        return { message: "user_exists" };
+      }
+      shouldRedirect = true;
+    } catch (err) {
+      console.error(err);
+    }
+    if (shouldRedirect) {
+      redirect("/home"); // try/catch문 안에서 X
+    }
   };
 
   return (
     <>
       <Modal>
         <Modal.Header title="계정을 생성하세요." />
-        <Modal.Form onSubmit={onSubmit}>
+        <Modal.Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            onSubmit(formData);
+          }}
+        >
           <Modal.FormWrapper>
             <Modal.Input
               label="아이디"
               id="id"
-              value={id}
-              onChange={onChangeId}
               type="text"
               placeholderText=""
             />
             <Modal.Input
               label="닉네임"
               id="name"
-              value={nickname}
-              onChange={onChangeNickname}
               type="text"
               placeholderText=""
             />
             <Modal.Input
               label="비밀번호"
               id="password"
-              value={password}
-              onChange={onChangePassword}
               type="password"
               placeholderText=""
             />
             <Modal.Input
               label="프로필"
               id="image"
-              onChange={onChangeImageFile}
-              previewUrl={image}
               type="file"
               accept="image/*"
               placeholderText=""
             />
           </Modal.FormWrapper>
           <Modal.Footer>
-            {message && <Modal.Message msg={message} />}
-            <Modal.Button title="가입하기" disabled={!id && !password} />
+            <Modal.Button title="가입하기" type="submit" />
           </Modal.Footer>
         </Modal.Form>
       </Modal>
