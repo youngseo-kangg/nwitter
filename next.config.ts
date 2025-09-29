b783ecf915c5e077b35dd11bdb119782b27180e3
Youngseo Kang <youngseo.kangg@gmail.com>
2025-09-29 10:15:25 +0900

    updating settings for server

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // 크기 제한 상향
+    },
+  },
 };
 
 export default nextConfig;
